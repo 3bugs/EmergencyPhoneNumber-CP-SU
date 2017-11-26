63d3388e4da28f9b06253a3616da75c9655275e6
Promlert Lovichit <promlert@gmail.com>
2017-11-26 16:04:32 +0700

    Add insert phone data

--- app/src/main/java/com/example/emergencyphonenumber/MainActivity.java
+++ app/src/main/java/com/example/emergencyphonenumber/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.emergencyphonenumber;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -8,6 +9,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.emergencyphonenumber.adapter.PhoneListAdapter;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase mDb;
 
     private ArrayList<PhoneItem> mPhoneItemList = new ArrayList<>();
+    private PhoneListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,34 +35,16 @@ public class MainActivity extends AppCompatActivity {
         mHelper = new PhoneDbHelper(this);
         mDb = mHelper.getReadableDatabase();
 
-        Cursor cursor = mDb.query(
-                PhoneDbHelper.TABLE_NAME,
-                null,
-                "category=1",
-                null,
-                null,
-                null,
-                null
-        );
+        loadDataFromDb();
 
-        while (cursor.moveToNext()) {
-            int id = cursor.getInt(cursor.getColumnIndex(PhoneDbHelper.COL_ID));
-            String title = cursor.getString(cursor.getColumnIndex(PhoneDbHelper.COL_TITLE));
-            String number = cursor.getString(cursor.getColumnIndex(PhoneDbHelper.COL_NUMBER));
-            String picture = cursor.getString(cursor.getColumnIndex(PhoneDbHelper.COL_PICTURE));
-
-            PhoneItem item = new PhoneItem(id, title, number, picture);
-            mPhoneItemList.add(item);
-        }
-
-        PhoneListAdapter adapter = new PhoneListAdapter(
+        mAdapter = new PhoneListAdapter(
                 this,
                 R.layout.item,
                 mPhoneItemList
         );
 
         ListView lv = findViewById(R.id.list_view);
-        lv.setAdapter(adapter);
+        lv.setAdapter(mAdapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -71,5 +57,59 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button insertButon = findViewById(R.id.insert_button);
+        insertButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText phoneTitleEditText = findViewById(R.id.phone_title_edit_text);
+                EditText phoneNumberEditText = findViewById(R.id.phone_number_edit_text);
+
+                // todo: เพิ่มการตรวจสอบ input
+
+                String phoneTitle = phoneTitleEditText.getText().toString();
+                String phoneNumber = phoneNumberEditText.getText().toString();
+
+                ContentValues cv = new ContentValues();
+                cv.put(PhoneDbHelper.COL_TITLE, phoneTitle);
+                cv.put(PhoneDbHelper.COL_NUMBER, phoneNumber);
+                cv.put(PhoneDbHelper.COL_PICTURE, "ic_launcher.png");
+
+                mDb.insert(PhoneDbHelper.TABLE_NAME, null, cv);
+                loadDataFromDb();
+                mAdapter.notifyDataSetChanged();
+
+                mDb.delete(
+                        PhoneDbHelper.TABLE_NAME,
+                        "title=? AND number=?",
+                        new String[]{"แจ้งเหตุด่วนเหตุร้าย", "199"}
+                );
+            }
+        });
+
+    } // ปิดเมธอด onCreate
+
+    private void loadDataFromDb() {
+        Cursor cursor = mDb.query(
+                PhoneDbHelper.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        mPhoneItemList.clear();
+
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(cursor.getColumnIndex(PhoneDbHelper.COL_ID));
+            String title = cursor.getString(cursor.getColumnIndex(PhoneDbHelper.COL_TITLE));
+            String number = cursor.getString(cursor.getColumnIndex(PhoneDbHelper.COL_NUMBER));
+            String picture = cursor.getString(cursor.getColumnIndex(PhoneDbHelper.COL_PICTURE));
+
+            PhoneItem item = new PhoneItem(id, title, number, picture);
+            mPhoneItemList.add(item);
+        }
     }
-}
+} // ปิดคลาส MainActivity
